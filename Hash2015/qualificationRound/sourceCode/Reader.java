43c70bb9e8f0a535ca3084fba023bedfb006a767
TaraBC <TaraBC@users.noreply.github.com>
2019-06-03 22:27:56 +0100

    Made a start on reading from files and an input/output class

--- Hash2015/qualificationRound/sourceCode/Reader.java
+++ Hash2015/qualificationRound/sourceCode/Reader.java
@@ -1,4 +1,27 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Reader {
-
+	/*Create a BufferedReader object*/
+	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	
+	/*CONSTRUCTOR METHOD*/
+	public Reader() {
+		
+	}
+	
+	/*KEYBOARD READER*/
+	public String keyboardReader(String inputText) throws IOException{
+		System.out.println(inputText);
+		return reader.readLine();
+	}
+	
+	public void fileReader(String filePath) throws IOException {
+		String st; 
+		while ((st = this.reader.readLine()) != null) {
+			System.out.println(st); 
+		}
+	}
 }
--- Hash2015/qualificationRound/sourceCode/main.java
+++ Hash2015/qualificationRound/sourceCode/main.java
@@ -1,7 +1,11 @@
+import java.io.IOException;
+
 public class main {
 	private Server[] servers;
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		Reader IO = new Reader();
+		String path = IO.keyboardReader("Please enter filepath to txt file with data centre details");
+		IO.fileReader(path);
 	}
 
 }
